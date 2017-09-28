bccf42b4db72cc875d657fa41e2601bcc2df7a5d
Sigma88 <sigmamodexpansions@outlook.com>
2017-09-28 16:39:56 +0200

    loading screen

---
+++ [Source]/GroundStretch/LoadingScreens.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+
+namespace PQSMod_GroundStretch
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class LoadingScreens : MonoBehaviour
+    {
+        bool skip = false;
+        Texture2D texture;
+
+        void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        void Start()
+        {
+            // Load the Texture
+            texture = LoadDDS(Resources.SigmaGSLS_1);
+        }
+
+        void Update()
+        {
+            // Wait for the Loading Screens to be available
+            if (!skip && LoadingScreen.Instance?.Screens?.Skip(1)?.FirstOrDefault() != null)
+            {
+                LoadScreen(LoadingScreen.Instance?.Screens?.Skip(1)?.FirstOrDefault());
+                skip = true;
+            }
+
+            if (HighLogic.LoadedScene == GameScenes.MAINMENU)
+            {
+                DestroyImmediate(this);
+            }
+        }
+
+        void LoadScreen(LoadingScreen.LoadingScreenState screen)
+        {
+            // Add the texture to the Loading Screens
+            List<Object> newScreens = screen.screens.ToList();
+            newScreens.Add(texture);
+            screen.screens = newScreens.ToArray();
+        }
+
+        Texture2D LoadDDS(byte[] bytes)
+        {
+            if (bytes[4] != 124) return null; //this byte should be 124 for DDS image files
+
+            int height = bytes[13] * 256 + bytes[12];
+            int width = bytes[17] * 256 + bytes[16];
+
+            int header = 128;
+            byte[] data = new byte[bytes.Length - header];
+            Buffer.BlockCopy(bytes, header, data, 0, bytes.Length - header);
+
+            Texture2D texture = new Texture2D(width, height, TextureFormat.DXT5, false);
+            texture.LoadRawTextureData(data);
+            texture.Apply();
+
+            return (texture);
+        }
+    }
+}
---
+++ [Source]/GroundStretch/Resources.Designer.cs
@@ -0,0 +1,36 @@
+﻿using System.Resources;
+
+
+namespace PQSMod_GroundStretch
+{
+    internal class Resources
+    {
+        private static ResourceManager resourceMan;
+
+        internal Resources()
+        {
+        }
+
+        internal static ResourceManager ResourceManager
+        {
+            get
+            {
+                if (object.ReferenceEquals(resourceMan, null))
+                {
+                    ResourceManager temp = new ResourceManager("PQSMod_GroundStretch.Resources", typeof(Resources).Assembly);
+                    resourceMan = temp;
+                }
+                return resourceMan;
+            }
+        }
+
+        internal static byte[] SigmaGSLS_1
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("SigmaGSLS_1");
+                return ((byte[])(obj));
+            }
+        }
+    }
+}
